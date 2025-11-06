9aa2acf745aa8023c1d37575357c874b261ec5d5
ruslanzhuminov@mail.ru <ruslanzhuminov@mail.ru>
2025-11-06 18:29:29 +0300

    lesson 5.2 rework

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/CategoryDbClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/CategoryDbClient.java
@@ -1,46 +1,50 @@
 package guru.qa.niffler.service;
 
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.data.dao.CategoryDao;
 import guru.qa.niffler.data.dao.impl.CategoryDaoJdbc;
 import guru.qa.niffler.data.entity.category.CategoryEntity;
+import guru.qa.niffler.data.tpl.JdbcTransactionTemplate;
 import guru.qa.niffler.model.CategoryJson;
 
 import java.util.List;
 import java.util.Optional;
 
-import static guru.qa.niffler.data.Databases.transaction;
 
 public class CategoryDbClient {
     private static final Config CFG = Config.getInstance();
+    private final CategoryDao categoryDao = new CategoryDaoJdbc();
+
+
+    private final JdbcTransactionTemplate jdbcTxTemplate = new JdbcTransactionTemplate(
+            CFG.spendJdbcUrl()
+    );
 
     public CategoryJson createCategory(CategoryJson categoryJson) {
-        return transaction(connection -> {
+        return jdbcTxTemplate.execute(() -> {
                     CategoryEntity category = CategoryEntity.fromJson(categoryJson);
-                    return CategoryJson.fromEntity(new CategoryDaoJdbc().create(category));
-                }, CFG.spendJdbcUrl()
+                    return CategoryJson.fromEntity(categoryDao.create(category));
+                }
         );
     }
 
     public Optional<CategoryJson> findCategoryByUserNameAndCategoryName(String userName, String categoryName) {
-        return transaction(connection -> {
-                    Optional<CategoryEntity> se = new CategoryDaoJdbc().findCategoryByUserNameAndCategoryName(userName, categoryName);
+        return jdbcTxTemplate.execute(() -> {
+                    Optional<CategoryEntity> se = categoryDao.findCategoryByUserNameAndCategoryName(userName, categoryName);
                     return se.map(CategoryJson::fromEntity);
-                }, CFG.spendJdbcUrl()
+                }
         );
     }
 
     public List<CategoryEntity> findAllByUserName(String userName) {
-        return transaction(connection -> {
-                    return new CategoryDaoJdbc().findAllByUserName(userName);
-                }, CFG.spendJdbcUrl()
+        return jdbcTxTemplate.execute(() -> {
+                    return categoryDao.findAllByUserName(userName);
+                }
         );
     }
 
     public void deleteCategory(CategoryEntity category) {
-        transaction(connection -> {
-                    new CategoryDaoJdbc().deleteCategory(category);
-                }, CFG.spendJdbcUrl()
-        );
+        categoryDao.deleteCategory(category);
     }
 
 }
